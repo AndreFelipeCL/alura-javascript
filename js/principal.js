9024a3440fd85f1b4211ffe5af00eae011d6aeea
André Felipe Camargo Leite <andrefelipecl@outlook.com>
2019-02-16 13:06:05 -0200

    Refatoração com alguns validadores.

--- js/principal.js
+++ js/principal.js
@@ -1,14 +1,81 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista"
 
-function calcIMC(){
-    var pacientes = document.querySelectorAll('.paciente');
-    pacientes.forEach(item => {
-        var peso = Math.abs(item.querySelector('.info-peso').innerText);
-        var altura = Math.abs(item.querySelector('.info-altura').innerText);
-        var imc = item.querySelector('.info-imc');
-        imc.innerText = (peso / (altura*altura)).toFixed(2);
-    });
-};
+const INVALID_IMC = "Valores incorretos para cálculo."
+var validadores = [];
 
-calcIMC();
+function validaPesoValueType(cliente){
+    if(cliente.peso !== Number){
+        return true;
+    }
+}
+validadores.push(validaPesoValueType);
+
+function validaPesoComum(cliente){
+    if(cliente.peso > 250){
+        return true;
+    }
+}
+validadores.push(validaPesoComum);
+
+function validaAlturaType(cliente){
+    if(cliente.altura !== Number){
+        return true;
+    }
+}
+validadores.push(validaAlturaType);
+
+function validaAlturaMenor(cliente){
+    if(cliente.altura <= 0.5){
+        return true;
+    }
+}
+validadores.push(validaAlturaMenor);
+
+function validaAlturaMaior(cliente){
+    if(cliente.altura > 3.0){
+        return true;
+    }
+}
+validadores.push(validaAlturaMaior);
+
+function clienteBuilder(peso, altura, imc) {
+    return {
+        "peso": peso,
+        "altura": altura,
+        "imc": imc
+    };
+}
+
+function setIMC(cliente, value) {
+    cliente.imc.innerText = value;
+}
+
+function validaValores(item){    
+    var cliente = clienteCriator(item);
+    
+    validadores.forEach(validador => {
+        if(validador(cliente)){
+            setIMC(cliente, INVALID_IMC);
+            return;
+        }
+        setIMC(cliente, doCalc(cliente));
+    });
+}
+
+function clienteCriator(htmlItem) {
+    var peso = Math.abs(htmlItem.querySelector('.info-peso').innerText);
+    var altura = Math.abs(htmlItem.querySelector('.info-altura').innerText);
+    var imc = htmlItem.querySelector('.info-imc');
+    return clienteBuilder(peso, altura, imc);
+}
+
+function doCalc(cliente) {
+    return (cliente.peso / Math.pow(cliente.altura, 2)).toFixed(2);
+}
+
+
+var pacientes = document.querySelectorAll('.paciente');
+pacientes.forEach(item => {
+    validaValores(item);
+});
