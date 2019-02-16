e83f878e03629ba52a8ae5641dfdaf6567a5d30b
André Felipe Camargo Leite <andrefelipecl@outlook.com>
2019-02-16 20:06:22 -0200

    Adicionando novos clientes com JS.

--- js/principal.js
+++ js/principal.js
@@ -66,10 +66,10 @@ function setIMC(cliente, value) {
 
 function validaValores(item){    
     var cliente = clienteCriator(item);
-    
     validadores.every(validador => {
         if(validador(cliente)){
             setIMC(cliente, INVALID_IMC);
+            item.classList.add('paciente-invalido');
             return false;
         } else {
             setIMC(cliente, doCalc(cliente));
@@ -89,8 +89,51 @@ function doCalc(cliente) {
     return (cliente.peso / Math.pow(cliente.altura, 2)).toFixed(2);
 }
 
+function init() {
+    var pacientes = document.querySelectorAll('.paciente');
+    pacientes.forEach(item => {
+        validaValores(item);
+    });
+}
 
-var pacientes = document.querySelectorAll('.paciente');
-pacientes.forEach(item => {
-    validaValores(item);
+var buttonAdd = document.querySelector('#adicionar-paciente');
+buttonAdd.addEventListener('click', function(event){
+    event.preventDefault();
+
+    var form = document.querySelector('#form-adiciona');
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+    
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add('paciente');
+    var nomeTd = document.createElement("td");
+    nomeTd.classList.add('info-nome')
+    var pesoTd = document.createElement("td");
+    pesoTd.classList.add('info-peso');
+    var alturaTd = document.createElement("td");
+    alturaTd.classList.add('info-altura');
+    var gorduraTd = document.createElement("td");
+    gorduraTd.classList.add('info-gordura')
+    var imcTd = document.createElement("td");
+    imcTd.classList.add('info-imc')
+    
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+    
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    validaValores(pacienteTr);
+    tabela.appendChild(pacienteTr);
 });
+
+init();
