5d619d82608221220f9b5b31bf5db2cf8dad42e9
André Felipe Camargo Leite <andrefelipecl@outlook.com>
2019-02-16 22:41:49 -0200

    Usando animações para remover

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -2,6 +2,9 @@ var table = document.querySelectorAll("table");
 
 table.forEach(function (table) {
     table.addEventListener("dblclick", function (event) {
-        event.target.parentNode.remove();
+        event.target.parentNode.classList.add("fadeOut");
+        setTimeout(function() {
+            event.target.parentNode.remove();
+        }, 500);
     });
 });
